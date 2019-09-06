d4473ca1d3ebfad802d9eac8c63e8bfed29e53be
Nyirate <rwakaemma34@gmail.com>
2019-09-06 22:36:14 +0200

    add new code

--- src/main/java/Encrypt.java
+++ src/main/java/Encrypt.java
@@ -1,2 +1,37 @@
+import java.util.Scanner;
+
 public class Encrypt {
+    public static void main(String[] args) {
+        String word;
+        int cle;
+        char sa;
+        String encryptedword = " ";
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a word: ");
+        word = sc.nextLine();
+
+        System.out.println("Enter key: ");
+        cle = sc.nextInt();
+
+        for (int i = 0; i < word.length(); ++i) {
+            sa = word.charAt(i);
+
+            if (sa >= 'a' && sa <= 'z') {
+                sa = (char) (sa + cle);
+
+                if (sa > 'z') {
+                    sa = (char) (sa - 'z' + 'a' - 1);
+                }
+
+                encryptedword += sa;
+
+            }
+        }
+    }
 }
+}
+
+
+
+
